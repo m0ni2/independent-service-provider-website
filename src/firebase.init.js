6faacb84fda33fbb89f6e0c0f073ed8e40da1631
Md. Monirul Islam <monir.rj@gmail.com>
2022-05-07 15:08:34 +0600

    env variable added

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,19 +1,23 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Import the functions you need from the SDKs you need
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBr_Tx5r8j9BHLtRfKKQvM0WtsUo0gRDQU",
-    authDomain: "perfect-wedding-photography.firebaseapp.com",
-    projectId: "perfect-wedding-photography",
-    storageBucket: "perfect-wedding-photography.appspot.com",
-    messagingSenderId: "588828475428",
-    appId: "1:588828475428:web:2fc0d9a68eaaa5f64b58b1"
+    apiKey: REACT_APP_apiKey,
+    authDomain: REACT_APP_authDomain,
+    projectId: REACT_APP_projectId,
+    storageBucket: REACT_APP_storageBucket,
+    messagingSenderId: REACT_APP_messagingSenderId,
+    appId: REACT_APP_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
 
 export default app;
